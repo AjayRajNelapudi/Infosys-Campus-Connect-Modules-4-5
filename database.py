b71217081c8a9212a0defc24157564d84a8c4463
AjayRajNelapudi <najayraj.16.cse@anits.edu.in>
2019-03-11 19:56:58 +0530

    Db updates

--- database.py
+++ database.py
@@ -13,4 +13,23 @@ class Bank_Database():
         self.conn = self.database.cursor()
         self.conn.execute('USE Bank')
 
+
+    def create_account(self, name, addr, password, account_type, account_balance):
+        try:
+            c_id = None
+            acc_no = None
+            customer_insert_query = "INSERT INTO Customer VALUES (%s, '%s', '%s', '%s')" % (c_id, password, name, addr)
+            account_insert_query = "INSERT INTO Account VALUES (%s, %s, '%s')" % (acc_no, account_balance, account_type)
+            self.conn.execute(customer_insert_query)
+            self.conn.execute(account_insert_query)
+
+            return (c_id, acc_no)
+        except:
+            return (None, None)
+
     def validate_login(self, c_id, password):
+        query = "SELECT * FROM Customer WHERE c_id = %s AND c_password = '%s'" % (c_id, password)
+        if self.conn.execute(query) == 1:
+            return True
+        return False
+
